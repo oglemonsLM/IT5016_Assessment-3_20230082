0c417898c1d1f0e349753ef02365f788882a287b
oglemonsLM <126117791+oglemonsLM@users.noreply.github.com>
2023-03-16 09:56:35 +1300

    Update hangman.py

--- hangman-master/hangman.py
+++ hangman-master/hangman.py
@@ -10,39 +10,46 @@ Github: https://www.github.com/kying18
 Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
 """
 
-import random #imports the random function to select words.
-from words import words #imports the list of words from the "words" file.
-from hangman_visual import lives_visual_dict #imports the ascii images from the "hangman_visual" file.
-import string #imports and allows the use of ascii letters later on.
+#imports the random function to select words.
+import random 
 
+#imports the list of words from the "words" file.
+from words import words 
 
+#imports the ascii images from the "hangman_visual" file.
+from hangman_visual import lives_visual_dict
+
+#imports and allows the use of ascii letters later on.
+import string
+
+#creates the function for get_valid_word so that a word may be assigned to it.
 def get_valid_word(words):
-    word = random.choice(words) #chooses a word from the list at random.
+    word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
     return word.upper()
 
-
+#creates the main function for choosing the word and assigning attributes like lives and and counts for letters in the word.
 def hangman():
     word = get_valid_word(words)
-    word_letters = set(word)  #gets how many letters there are in a word.
+    word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()  #what letters the user has guessed.
+    used_letters = set()
 
-    lives = 7 #sets amount of lives.
+    lives = 7
 
-    # getting user input
+    #a loop that will keep going until either the player has won or lost the game.
     while len(word_letters) > 0 and lives > 0:
-        # letters used
-        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        #shows the user amount of lives left and letter used.
         print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-        # what current word is (ie W - R D)
+        #shows the current word with guesed letters and tells the rest of the code to display the letters that haven't been guessed as a '-'.
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print(lives_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
 
+        #this statement will take players input and check it against used letters if it is not in the used letters it will check the word for that letter and decide if it needs to be added to either the word or the letters used list.
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
@@ -50,23 +57,26 @@ def hangman():
                 word_letters.remove(user_letter)
                 print('')
 
+            #takes away a life if a guess is wrong.
             else:
-                lives = lives - 1  #takes away a life if a guess is wrong.
+                lives = lives - 1 
                 print('\nYour letter,', user_letter, 'is not in the word.')
 
+        #this shows the player that if they guess a letter wrong they have to guess again.
         elif user_letter in used_letters:
             print('\nYou have already used that letter. Guess another letter.')
-
+        
+        #this is displayed to the user if the character they entered is not a letter or is not recognized by the code and to try again.
         else:
             print('\nThat is not a valid letter.')
 
-    # gets here when len(word_letters) == 0 OR when lives == 0
+    #the code gets to this point if there are either no lives remaining or the player has guessed the word.
     if lives == 0:
         print(lives_visual_dict[lives])
         print('You died, sorry. The word was', word)
     else:
         print('YAY! You guessed the word', word, '!!')
 
-
+#checking wether or not the file is running due to import.
 if __name__ == '__main__':
     hangman()
